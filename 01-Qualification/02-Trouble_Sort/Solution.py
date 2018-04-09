7d3c53abc755ba77f794a97047c27ca3efd440d6
Mushinako <ridoedee@gmail.com>
2018-04-09 13:51:31 -0700

    Small change
    May improve speed by 0.001%

--- 01-Qualification/02-Trouble_Sort/Solution.py
+++ 01-Qualification/02-Trouble_Sort/Solution.py
@@ -1,14 +1,14 @@
 t = int(input())
 
 for i in range(1, t+1):
-    input() # n, no use
+    n = input()
     v = [int(j) for j in input().split(' ')]
 
     # Separate the values on even and odd indexes
     v_ieven = []
     v_iodd = []
 
-    for k in range(len(v)):
+    for k in range(n):
         if k % 2:
             v_iodd.append(v[k])
         else:
@@ -19,7 +19,7 @@ for i in range(1, t+1):
 
     valid = True
 
-    for l in range(len(v)-1):
+    for l in range(n-1):
         if l % 2:
             if v_iodd[(l-1)//2] > v_ieven[(l+1)//2]:
                 valid = False
